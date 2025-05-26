8e1cb733abb698cdc3d3c7d59fc352e3f599ecb5
Brady Yang <brady_yang@wistron.com>
2025-05-26 22:59:10 +0800

    add hash router to avoid 404 error

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,12 @@
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 
-import App from './App.jsx';
-import './index.css';
+import App from "./App.jsx";
+import "./index.css";
+// index.js or App.js
+import { HashRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <HashRouter>
+    <App />
+  </HashRouter>
+);
